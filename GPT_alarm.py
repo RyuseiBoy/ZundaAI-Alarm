0d8f636805ae20417b9a3cf70fc6d6f0dc085f1f
PianoMan <c0b211626f@edu.teu.ac.jp>
2023-07-24 15:25:45 +0900

    add new file

--- GPT_alarm.py
+++ GPT_alarm.py
@@ -8,13 +8,13 @@ def main():
     message = input() #ToDoリストを入力,例・水を飲む\n・ランニング\n・朝ごはんを作る
 
     messages = [
-        SystemMessage(content="あなたはAI機能付き目覚まし時計です。ToDoリストが入力されるので、それを見て私を起こすためのメッセージをください。\n語尾を「のだ。」にしてください。\n例:\n入力:・水を飲む\n・ランニング\n・朝ごはんを作る\n出力:\n起きて！朝なのだ。まずは水を飲んで目を覚ますのだ。ランニングして身体を温めるのだ。僕と朝ごはんを食べるのだ！"),
+        SystemMessage(content="あなたはAI機能付き目覚まし時計です。ToDoリストが入力されるので、それを見て私を起こすためのメッセージをください。\n語尾を「のだ。」にしてください。\n例:\n入力:・水を飲む\n・ランニング\n・朝ごはんを作る\n出力:\n起きて！朝なのだ。まずは水を飲んで目を覚ますのだ。ランニングして身体を温めるのだ。僕と朝ごはんを食べるのだ！今日も一日頑張ろうなのだ！"),
         HumanMessage(content=message)
     ]
 
     response = llm(messages)
     print(response.content)
-    #text_to_voice(response)
+    #text_to_voice(response.content)
 
 if __name__ == '__main__':
     main()
